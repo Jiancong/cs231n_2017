9cc506a12789ce0d6b246e81bb3d410159a63eac
Jiancong <94389147@qq.com>
2018-07-09 16:17:01 +0800

    RMProp and Adam update method.

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,7 +65,12 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+
+    # Momentum update
+    next_w = w
+    v = config['momentum'] * v - config['learning_rate'] * dw # integrate velocity
+    next_w += v # integrate position
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -99,7 +104,10 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    next_x = x
+
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
+    next_x += - config['learning_rate'] * dx / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -136,7 +144,10 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    next_x = x
+    config['m'] = config['beta1']*config['m'] + (1-config['beta1'])*dx
+    config['v'] = config['beta2']*config['v'] + (1-config['beta2'])*(dx**2)
+    next_x += - config['learning_rate'] * config['m'] / (np.sqrt(config['v']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
